dc7e65562653a4984f6f9c5a085510dc295a2124
ovice-site <156383708+ovice-site@users.noreply.github.com>
2024-01-15 11:59:06 +0900

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,78 +1,111 @@
-<script>
-// by Tok
+// by Tok@ovice, 2024
 var global_prm;
+var global_prm_val;
 var global_prf_country = "en";
 var global_btn_position = "";
+const className_UX_for_AU = "ux_for_au";
+const className_UX_for_EN = "ux_for_en";
+const className_trial_button = "trial_button";
 
 function retrieveGETqs() {
 	var query = window.location.search.substring(1);
-	return query;
 	if (!query) return false;
+	return query;
 }
 
 function getUserLangByUA() {
   return window.navigator.language;
 }
 
-function getUserLangByGL() {
+function getUserLangByGLwithUX() {
   const Http = new XMLHttpRequest();
   var bdcApi = "https://api.bigdatacloud.net/data/reverse-geocode-client";
-  var gl;
 
-  console.log("get");
     navigator.geolocation.getCurrentPosition(
       (position) => {
         bdcApi = bdcApi
           + "?latitude=" + position.coords.latitude
           + "&longitude=" + position.coords.longitude
           + "&localityLanguage=en";
-        gl = getbdcApi(bdcApi);
+        getbdcApi(bdcApi);
       },
-      (err) => { gl = getbdcApi(bdcApi); },
+      (err) => { getbdcApi(bdcApi); },
       {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
       }
     );
-  console.log("done"); console.log(gl);
 
   function getbdcApi(bdcApi) {
-  console.log("func begin");
   var data;
   var bdc = new Promise((resolve, reject) => {
     Http.open("GET", bdcApi);
     Http.send();
     Http.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-        console.log("return"); console.log(JSON.parse(this.responseText));
         data = JSON.parse(this.responseText);
         resolve();
       }
     };
   });
-  bdc.then(function(value) {console.log("then"); console.log(data);});
-  console.log("func end");
+  bdc.then(function(value) {
+    global_prf_country = data.countryCode;
+    console.log("country code = "); console.log(global_prf_country);
+    UXcustomizeViaCountry();
+  });
+  }
+}
+
+function UXinitialize(){
+  var UX_for_AU = document.getElementsByClassName(className_UX_for_AU);
+  for (var i = 0; i < UX_for_AU.length; i++) {
+    UX_for_AU[i].style.display = 'none';
+  }
+  var UX_for_EN = document.getElementsByClassName(className_UX_for_EN);
+  for (var i = 0; i < UX_for_EN.length; i++) {
+    UX_for_EN[i].style.display = 'none';
+  }
+}
+
+function UXcustomizeViaCountry(){
+  var UX_for_AU = document.getElementsByClassName(className_UX_for_AU);
+  for (var i = 0; i < UX_for_AU.length; i++) {
+    switch (global_prf_country) {
+      case 'EN_AU':
+      case 'en-AU':
+        UX_for_AU[i].style.display = 'block';
+        break;
+      default:
+        UX_for_AU[i].style.display = 'none';
+    }
+  }
+  var UX_for_EN = document.getElementsByClassName(className_UX_for_EN);
+  for (var i = 0; i < UX_for_EN.length; i++) {
+    switch (global_prf_country) {
+      case 'EN':
+      case 'en':
+        UX_for_EN[i].style.display = 'block';
+        break;
+      default:
+        UX_for_EN[i].style.display = 'none';
+    }
   }
 }
 
 (function(){
   var str = retrieveGETqs();
   global_prm = decodeURIComponent(str);
+  global_prm_val = new URLSearchParams(global_prm);
 
-//  global_prf_country = getUserLangByUA();
-  global_prf_country = getUserLangByGL();
-  console.log(global_prf_country);
-  var UX_for_AU = document.getElementsByClassName("UX_for_AU");
-  for (var i = 0; i < UX_for_AU.length; i++) {
-    switch (global_prf_country) {
-      case 'EN_AU':
-        UX_for_AU[i].style.visibility = 'visible';
-        break;
-      default:
-        UX_for_AU[i].style.visibility = 'hidden';
-    }
+  UXinitialize();
+  if (global_prm_val.has("country")) {
+    global_prf_country = getUserLangByUA();
+    UXcustomizeViaCountry();
+  } else {
+    getUserLangByGLwithUX();
   }
+
 })();
 
 $(function(){
@@ -89,22 +122,10 @@ $(function(){
         $('a').attr('href', target_url + '?' + global_prm);
       }
     }
-  console.log(global_prm);
-  console.log(target_url);
   })
 })
-</script>
 
-//
-//
-//
-<script>
-
-// test 3
-$('.trial-button').click(function(e) {
- global_btn_position = e.currentTarget.dataset['trial'];
- console.log(global_btn_position);
- jQuery('#experiment-text3').text('position of button: ' + global_btn_position);
+$('.' + className_trial_button).click(function(e) {
+  global_btn_position = e.currentTarget.dataset['trial'];
+  console.log(global_btn_position);
 });
-
-</script>
