7e0c28239eafe3f507ef4482aa330993b55900d8
ovice-site <156383708+ovice-site@users.noreply.github.com>
2024-05-21 17:52:38 +0900

    build 028 update refdata and default value on return

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
-// ovice utils build 027 by Tok@ovice, 2024 
-var global_utils = 27;
+// ovice utils build 028 by Tok@ovice, 2024 
+var global_utils = 28;
 var global_prm;
 var global_prm_val;
 var global_prf_country = 'none';
@@ -145,64 +145,63 @@ function UXcustomizeViaCountry(){
 
 function checkAttribution(d) {
   const refdata = `[
-    {"domain": "ovice.", "ref": "ovice"},
-    {"domain": "flexergylab.com", "ref": "ovice"},
-    {"domain": "play.google.com", "ref": "aso_google"},
-    {"domain": "apps.apple.com", "ref": "aso_apple"},
-    {"domain": "google.", "ref": "seo_google"},
-    {"domain": "yahoo.co.jp", "ref": "seo_yahoo"}, 
-    {"domain": "yahoo.com", "ref": "seo_yahoo"}, 
-    {"domain": "bing.com", "ref": "seo_bin"},
-    {"domain": "duckduckgo.com", "ref": "seo_duckduckgo"},
-    {"domain": "coccoc.com", "ref": "seo_coccoc"},
-    {"domain": "yandex.com", "ref": "seo_yandex"},
-    {"domain": "naver.com", "ref": "seo_naver"},
-    {"domain": "baidu.com", "ref": "seo_baidu"},
-    {"domain": "ecosia.org", "ref": "seo_ecosia"},
-    {"domain": "msn.com", "ref": "seo_msn"},
-    {"domain": "excite.co.jp", "ref": "seo_excite"},
-    {"domain": "goo.ne.jp", "ref": "seo_goo"},
-    {"domain": "livedoor.com", "ref": "seo_livedoor"},
-    {"domain": "biglobe.ne.jp", "ref": "seo_biglobe"},
-    {"domain": "ocn.ne.jp", "ref": "seo_ocn"},
-    {"domain": "nifty.com", "ref": "seo_nifty"},
-    {"domain": "infoseek.co.jp", "ref": "seo_infoseek"},
-    {"domain": "auone.jp", "ref": "seo_au"},
-    {"domain": "docomo.ne.jp", "ref": "seo_docomo"},
-    {"domain": "facebook.com", "ref": "social_facebook"},
-    {"domain": "x.com", "ref": "social_x"},
-    {"domain": "linkedin.com", "ref": "social_linkedin"},
-    {"domain": "youtube.com", "ref": "social_youtube"},
-    {"domain": "instagram.com", "ref": "social_instagram"},
-    {"domain": "www.itreview.jp", "ref": "review_itreview"},
-    {"domain": "boxil.jp", "ref": "review_boxil"},
-    {"domain": "it-trend.jp", "ref": "review_it-trend"},
-    {"domain": "capterra.jp", "ref": "review_capterra"},
-    {"domain": "capterra.com", "ref": "review_capterra"},
-    {"domain": "g2.com", "ref": "review_g2"},
-    {"domain": "getapp.com", "ref": "review_getapp"},
-    {"domain": "prtimes.jp", "ref": "media_prtimes"},
-    {"domain": "wantedly.com", "ref": "media_wantedly"},
-    {"domain": "techable.jp", "ref": "media_techable"},
-    {"domain": "zendesk.com", "ref": "media_zendesk"},
-    {"domain": "patentsalon.com", "ref": "media_patentsalon"},
-    {"domain": "note.com", "ref": "media_note"},
-    {"domain": "connpas.com", "ref": "media_connpas"},
-    {"domain": "voice-ping.com", "ref": "media_voice-ping"},
-    {"domain": "toremaga.com", "ref": "media_toremaga"},
-    {"domain": "wmr.tokyo", "ref": "media_wmr-tokyo"},
-    {"domain": "zdnet.com", "ref": "media_zdnet"},
-    {"domain": "cnet.com", "ref": "media_cnet"},
-    {"domain": "impress.co.jp", "ref": "media_impress"},
-    {"domain": "panora.tokyo", "ref": "media_panora-tokyo"},
-    {"domain": "notion.site", "ref": "media_notion"}
+    {"domain": "play.google.com", "ref": "aso_gosea"},
+    {"domain": "apps.apple.com", "ref": "aso_apsea"},
+    {"domain": "google.", "ref": "seo_gosea"},
+    {"domain": "yahoo.co.jp", "ref": "seo_yasea"}, 
+    {"domain": "yahoo.com", "ref": "seo_yasea"}, 
+    {"domain": "bing.com", "ref": "seo_misea"},
+    {"domain": "duckduckgo.com", "ref": "seo_otsea"},
+    {"domain": "coccoc.com", "ref": "seo_otsea"},
+    {"domain": "yandex.com", "ref": "seo_otsea"},
+    {"domain": "naver.com", "ref": "seo_otsea"},
+    {"domain": "baidu.com", "ref": "seo_otsea"},
+    {"domain": "ecosia.org", "ref": "seo_otsea"},
+    {"domain": "msn.com", "ref": "seo_otsea"},
+    {"domain": "excite.co.jp", "ref": "seo_otsea"},
+    {"domain": "goo.ne.jp", "ref": "seo_otsea"},
+    {"domain": "livedoor.com", "ref": "seo_otsea"},
+    {"domain": "biglobe.ne.jp", "ref": "seo_otsea"},
+    {"domain": "ocn.ne.jp", "ref": "seo_otsea"},
+    {"domain": "nifty.com", "ref": "seo_otsea"},
+    {"domain": "infoseek.co.jp", "ref": "seo_otsea"},
+    {"domain": "auone.jp", "ref": "seo_otsea"},
+    {"domain": "docomo.ne.jp", "ref": "seo_otsea"},
+    {"domain": "facebook.com", "ref": "soc_fapos"},
+    {"domain": "x.com", "ref": "soc_xpos"},
+    {"domain": "linkedin.com", "ref": "soc_lipos"},
+    {"domain": "youtube.com", "ref": "soc_yopos"},
+    {"domain": "instagram.com", "ref": "ref_otart"},
+    {"domain": "www.itreview.jp", "ref": "ref_itart"},
+    {"domain": "prtimes.jp", "ref": "ref_prart"},
+    {"domain": "boxil.jp", "ref": "ref_otart"},
+    {"domain": "it-trend.jp", "ref": "ref_otart"},
+    {"domain": "capterra.jp", "ref": "ref_otart"},
+    {"domain": "capterra.com", "ref": "ref_otart"},
+    {"domain": "g2.com", "ref": "ref_otart"},
+    {"domain": "getapp.com", "ref": "ref_otart"},
+    {"domain": "wantedly.com", "ref": "ref_otart"},
+    {"domain": "techable.jp", "ref": "ref_otart"},
+    {"domain": "zendesk.com", "ref": "ref_otart"},
+    {"domain": "patentsalon.com", "ref": "ref_otart"},
+    {"domain": "note.com", "ref": "ref_otart"},
+    {"domain": "connpas.com", "ref": "ref_otart"},
+    {"domain": "voice-ping.com", "ref": "ref_otart"},
+    {"domain": "toremaga.com", "ref": "ref_otart"},
+    {"domain": "wmr.tokyo", "ref": "ref_otart"},
+    {"domain": "zdnet.com", "ref": "ref_otart"},
+    {"domain": "cnet.com", "ref": "ref_otart"},
+    {"domain": "impress.co.jp", "ref": "ref_otart"},
+    {"domain": "panora.tokyo", "ref": "ref_otart"},
+    {"domain": "notion.site", "ref": "ref_otart"}
   ]`;
   var j = JSON.parse(refdata);
   for (var i = 0; i < j.length; i++) {
     if (d.indexOf(j[i].domain) > -1) {return j[i].ref;}
   }
   const u = new URL(d);
-  return 'other_' + u.host;
+//  return 'other_' + u.host;
+  return 'ref_otart';
 }
 
 function secdomain(p) {
